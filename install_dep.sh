2a683f71072a23c181bc91c7286fa7bf87b0f28d
gastrodia <nil@sanwu.org>
2017-11-12 20:26:55 +0800

    install.sh

---
+++ install_dep.sh
@@ -0,0 +1,4 @@
+
+  meteor npm install --save airbnb-prop-types is-touch-device consolidated-events react-portal react-moment-proptypes object.assign object.values deep-equal
+  meteor npm install --save core-js
+
---
+++ start.sh
@@ -0,0 +1,2 @@
+meteor  --port 127.0.0.1:3389 
+#meteor --production --port 127.0.0.1:3389
